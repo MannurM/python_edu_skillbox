9fd6ffd294a238a984dbc22f740249772330bf7b
Mannur <zmannur@gmail.com>
2020-05-01 07:21:29 +0400

    Заменил Гоблина на Волка Заменил пружинку на веревочку Сделано

--- lesson_002/00_distance.py
+++ lesson_002/00_distance.py
@@ -12,9 +12,27 @@ sites = {
 # Составим словарь словарей расстояний между ними
 # расстояние на координатной сетке - ((x1 - x2) ** 2 + (y1 - y2) ** 2) ** 0.5
 
-distances = {}
+distances = dict()
 
-# TODO здесь заполнение словаря
+moscow = sites['Moscow']
+london = sites['London']
+paris = sites['Paris']
+
+moscow_london = ((moscow[0] - london[0]) ** 2 + (moscow[1]-london[1]) ** 2) ** 0.5
+moscow_paris = ((moscow[0] - paris[0]) ** 2 + (moscow[1]-paris[1]) ** 2) ** 0.5
+london_paris = ((london[0] - paris[0]) ** 2 + (london[1] - paris[1]) ** 2) ** .5
+
+distances['Moscow'] = {}
+distances['Moscow']['London'] = moscow_london
+distances['Moscow']['Paris'] = moscow_paris
+
+distances['London'] = {}
+distances['London']['Moscow'] = moscow_london
+distances['London']['Paris'] = london_paris
+
+distances['Paris'] = {}
+distances['Paris']['London'] = london_paris
+distances['Paris']['Moscow'] = moscow_paris
 
 print(distances)
 
