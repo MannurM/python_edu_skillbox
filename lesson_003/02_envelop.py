8d2a8279a0e2841e2216799acbf34f38f02fe1d9
Mannur <zmannur@gmail.com>
2020-05-27 20:01:10 +0400

    Сделал 02 в Lesson 3

--- lesson_003/02_envelop.py
+++ lesson_003/02_envelop.py
@@ -11,25 +11,23 @@
 # Использовать только операторы if/elif/else, можно вложенные
 
 envelop_x, envelop_y = 10, 7
-paper_x, paper_y = 8, 9
+paper_x, paper_y = 0, 0
 # проверить для
 paper_x, paper_y = 9, 8
-if envelop_x >= paper_x or envelop_x >= paper_y:  # TODO Такие условия не подойдут.
-    # TODO Они пропустят любой лист, если одна из его сторон подходит, т.е. лист размером 1, 10000 этот алгоритм
-    # TODO пропустит, хотя должен отклонить.
-    if envelop_y >= paper_x or envelop_y >= paper_y:
+if envelop_x >= paper_x and envelop_x >= paper_y:
+    if envelop_y >= paper_x and envelop_y >= paper_y:
         print('Да')
     else:
         print('Нет')
 paper_x, paper_y = 6, 8
-if envelop_x >= paper_x or envelop_x >= paper_y:
-    if envelop_y >= paper_x or envelop_y >= paper_y:
+if envelop_x >= paper_x and envelop_x >= paper_y:
+    if envelop_y >= paper_x and envelop_y >= paper_y:
         print('Да')
     else:
         print('Нет')
 paper_x, paper_y = 8, 6
-if envelop_x >= paper_x or envelop_x >= paper_y:
-    if envelop_y >= paper_x or envelop_y >= paper_y:
+if envelop_x >= paper_x and envelop_x >= paper_y:
+    if envelop_y >= paper_x and envelop_y >= paper_y:
         print('Да')
     else:
         print('Нет')
@@ -45,14 +43,14 @@ else:
     print('Неееет')
 
 paper_x, paper_y = 11, 9
-if envelop_x >= paper_x or envelop_x >= paper_y:
-    if envelop_y >= paper_x or envelop_y >= paper_y:
+if envelop_x >= paper_x and envelop_x >= paper_y:
+    if envelop_y >= paper_x and envelop_y >= paper_y:
         print('Да')
     else:
         print('Нет')
 paper_x, paper_y = 9, 11
-if envelop_x >= paper_x or envelop_x >= paper_y:
-    if envelop_y >= paper_x or envelop_y >= paper_y:
+if envelop_x >= paper_x and envelop_x >= paper_y:
+    if envelop_y >= paper_x and envelop_y >= paper_y:
         print('Да')
     else:
         print('Нет')
@@ -60,15 +58,16 @@ if envelop_x >= paper_x or envelop_x >= paper_y:
 
 print("закончились конверты")
 
-
 # Усложненное задание, решать по желанию.
 # Заданы размеры hole_x, hole_y прямоугольного отверстия и размеры brick_х, brick_у, brick_z кирпича (все размеры
 # могут быть в диапазоне от 1 до 1000)
 #
 # Определить, пройдет ли кирпич через отверстие (грани кирпича параллельны сторонам отверстия)
 
-# hole_x, hole_y = 8, 9
-# brick_x, brick_y, brick_z = 11, 10, 2
+hole_x, hole_y = 0, 0
+brick_x, brick_y, brick_z = 0, 0, 0
+
+
 # brick_x, brick_y, brick_z = 11, 2, 10
 # brick_x, brick_y, brick_z = 10, 11, 2
 # brick_x, brick_y, brick_z = 10, 2, 11
@@ -94,59 +93,25 @@ print("закончились конверты")
 # могут быть в диапазоне от 1 до 1000)
 #
 # Определить, пройдет ли кирпич через отверстие (грани кирпича параллельны сторонам отверстия)
+# TODO:  я понял нужно чтобы было использование elif
+# TODO: не знаю стоит ли выделять блок для печати - отдельную подпрограмму (функцию)
+# TODO: def print_answer_brick_in_hole():
+# TODO:     print('Да')
+
 
 def brick_in_hole(hole_x, hole_y, brick_x, brick_y, brick_z):
-    hole = 0
-    brick_1 = 0
-    brick_2 = 0
-    brick_3 = 0
+    if hole_x >= brick_x and hole_y >= brick_y:
+        print(brick_x, brick_y, brick_z, 'Да')  # def print_answer_brick_in_hole():
 
-    # hole = hole_x + hole_y
-    # brick_1 = brick_x + brick_y
-    # brick_2 = brick_x + brick_z
-    # brick_3 = brick_y + brick_z
-    #
-    # if hole >= brick_1 and hole >= brick_2 and hole >= brick_3:
-    #     print('Да')
-    #
-    # else:
-    #     print('Нет')
-    #
-    # первоначально было так, потом сказали можно проще - без расчетов, теперь опять считать.
-    # Да мне тоже эта лесенка не нравилась.
-    # Можно и с минусами!!!
-    # result1 = 0
-    # result2 = 0
-    # result3 = 0
-    # result1 = hole - brick_1
-    # result2 = hole - brick_2
-    # result3 = hole - brick_3
-    #
-    # if result1 >= 0 and result2 >= 0 and result3 >= 0:
-    #     print('Размеры', brick_x, brick_y, brick_z, 'Да')
-    # else:
-    #     print('Размеры', brick_x, brick_y, brick_z,'Нет')
-    # мой вариант годится только для целых чисел
+    elif hole_x >= brick_x and hole_y >= brick_z:
+        print(brick_x, brick_y, brick_z, 'Да')
 
-    # Вариант с площадями годится и для дробных значений размеров кирпича
-    # а зачем 6 площадей? они же попарно равны
-    hole = hole_x * hole_y
-    brick_1 = brick_x * brick_y
-    brick_2 = brick_x * brick_z
-    brick_3 = brick_y * brick_z
-    # TODO Площадь в данном случае не даст правильный ответ.
-    # TODO Возьмем крайний случай, когда размер отверстия X равен 1000, а размер отверстия Y равен 1
-    # TODO Площадь будет равна 1000, но кирпич с размерами 10х10х10 не влезет в это отверстие
-    # TODO как бы его не поворачивали.
-    # TODO Поэтому нужно проверить 6 сочетаний сторон, о которых я писал в прошлый раз
-    if hole >= brick_1 and hole >= brick_2 and hole >= brick_3:
-        print('Да')
+    elif hole_x >= brick_y and hole_y >= brick_z:
+        print(brick_x, brick_y, brick_z, 'Да')
 
     else:
-        print('Нет')
+        print(brick_x, brick_y, brick_z, 'Нет')
 
-
-print('усложненный блок')
 
 hole_x, hole_y = 8, 9
 
@@ -155,7 +120,7 @@ brick_x, brick_y, brick_z = 8.1, 8.1, 8.9
 brick_in_hole(hole_x, hole_y, brick_x, brick_y, brick_z)
 # не пролезет
 
-brick_x, brick_y, brick_z = 8, 8, 9
+brick_x, brick_y, brick_z = 9, 8, 8
 brick_in_hole(hole_x, hole_y, brick_x, brick_y, brick_z)
 
 brick_x, brick_y, brick_z = 9, 9, 0
