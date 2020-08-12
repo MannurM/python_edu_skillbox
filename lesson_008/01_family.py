3777dd6e4aebae30c00fd3c96c1cea240a6a3196
Максим <bi4ok23@gmail.com>
2020-08-12 13:48:24 +0300

    lesson 008: зачёт

--- lesson_008/01_family.py
+++ lesson_008/01_family.py
@@ -553,3 +553,4 @@ cprint(home.results(), color='green')
 # #       for salary in range(50, 401, 50):
 # #           max_cats = life.experiment(salary)
 # #           print(f'При зарплате {salary} максимально можно прокормить {max_cats} котов')
+#зачёт!
