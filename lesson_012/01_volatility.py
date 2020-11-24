4a892c9e0205020f1cd1b757cc4fec5d254d8d22
Mannur <zmannur@gmail.com>
2020-11-24 13:53:52 +0400

    12.1 нужны правки

--- lesson_012/01_volatility.py
+++ lesson_012/01_volatility.py
@@ -65,149 +65,44 @@
 #
 # Для плавного перехода к мультипоточности, код оформить в обьектном стиле, используя следующий каркас
 #
-import csv
 import os
+import prepare
 
 
-# import operator
+def file_to_path(folder_name):  # путь до файла
+    file_in_folder = os.listdir(folder_name)
+    path = os.getcwd() + '/' + folder_name
+    for file in file_in_folder:
+        file_path = path + '/' + file
+        file_path = os.path.normpath(file_path)
+        print('путь к файлу есть!')
+        volatil.run(file_path=file_path)
+    return
 
 
-class Volatility:
-    # TODO Класс надо заточить под обработку одного файла, а вне класса пройтись по директории
-    # TODO И для каждого файла создать по объекту для расчётов
-    # TODO Потом пройти по всем объектам и собрать результаты вместе.
-    # TODO Эти все сложности помогут легче выполнить два следующих задания)
-
-    # TODO Ещё было бы удобно выделить сортировку и печать в отдельную функцию
-    # TODO И ещё одну функцию-генератор создать, которая на вход будет получать путь к директории
-    # TODO А на выход будет выдавать путь к файлу из директории
-    # TODO Эти две функции можно будет вынести в отдельный модуль и импортировать в каждое из заданий этого модуля
-
+class VolatilityObject:
 
     def __init__(self):  # <сохранение параметров>
-        # TODO слишком много лишних параметров
-        # TODO удалите всё, что сможете
-        # TODO Оставьте только то, что подходит под условия:
-        # TODO 1) Используется больше, чем в одном методе
-        # TODO 2) К этому нужно сохранить доступ после работы объетка
-        self.dir_name = None
-        self.secid = None  # имя тикера
-        self.tiker_time = None  # время тикера
-        self.tiker_quantity = None  # количество проданного
-        self.tiker_price = []
-        self.file_in_folder = 0  # количества файлов в исходной папке
-        self.volatility_rezult = 0
-        self.volatility_max = []
-        self.volatility_min = []
-        self.volatility_dict = {}
-        self.volatility_zero = []
-        self.tiker_price_list = []  # список значений из файла
-        self.half_sum = 0  # полусумма
-        self.difference = 0  # разность макс и мин
-        self.value_max = 0
-        self.value_min = 0
         self.dict_file = {}
-        self.key = None
-        # self.param_dir = param_dir  # Путь до папки с файлами?
+        self.volatility_dict = {}
+        self.file_path = ''
 
-    def run(self, dir_name):  # <обработка данных>
-        self.dir_name = dir_name
-        self.file_in_folder = os.listdir(self.dir_name)
-        path = os.getcwd() + '\\' + self.dir_name
-        path = os.path.normpath(path)
+    def run(self, file_path):  # <обработка данных>
+        print('1', file_path)
+        self.dict_file = prepare.PrepareObject.prepare(file_path=file_path)
+        print('2')
+        self.volatility_dict = self.prepare.PrepareObject.calculation(self.dict_file)
+        print('3')
+        self.prepare.PrepareObject.printed_rezult(self.volatility_dict)
 
-        for file in self.file_in_folder:
-            print(file)
-            file = path + '\\' + file
-            self.prepare(file)
-            self.tiker_price_list = self.dict_file[self.secid]
-            sorted(self.tiker_price_list)  # TODO эта строка не изменяет self.tiker_price_list
-            # TODO функция sorted возвращает копию объекта в отсортированном виде, но вы эту копию не сохраняете
-            self.value_max = float(max(self.tiker_price_list))
-            self.value_min = float(min(self.tiker_price_list))
-            if self.value_max == self.value_min:
-                self.volatility_zero.append(self.secid)
-            self.half_sum = (self.value_max + self.value_min) / 2
-            self.difference = (self.value_max - self.value_min)
-            self.difference = round(self.difference, 4)
-            self.volatility_rezult = (self.difference / self.half_sum) * 100
-            self.volatility_rezult = round(self.volatility_rezult, 4)
-            self.volatility_dict.update({self.secid: self.volatility_rezult})
-            print('____________', self.value_max, self.value_min, self.half_sum, self.difference,
-                  self.volatility_rezult)
 
-            self.tiker_price_list = []
-            self.value_max = 0
-            self.value_min = 0
-            self.volatility_rezult = 0
-            self.half_sum = 0
-            self.difference = 0
+volatil = VolatilityObject()
 
-        # print(self.volatility_dict)
+if __name__ == '__main__':
+    file_path = file_to_path(folder_name='trades')
 
-        # self.volatility_max = list(self.volatility_dict.items(), key=operator.itemgetter(1))
-        for i, v in self.volatility_dict.items():
-            self.volatility_max.append(v)
-        self.volatility_min = self.volatility_max
-        #  Вывод на консоль
-        self.volatility_max.sort(reverse=True)
-        print('Максимальная волатильность:')
-        for i in range(3):
-            self.get_value(dic=self.volatility_dict, value=self.volatility_max[i])
-            print(self.key, self.volatility_max[i])
+    # print('путь к файлу есть!')
+    # volatil.run(file_path=file_path)
 
-        self.volatility_min.sort()
-        print('Минимальная волатильность:')
-        for i in range(3):
-            self.get_value(dic=self.volatility_dict, value=self.volatility_min[i])
-            print(self.key, self.volatility_min[i])
-
-        self.volatility_zero.sort()
-        print('Нулевая волатильность:')
-        print(self.volatility_zero)
-
-        # Запустил препаре
-        # получил словарь из препаре
-        # взял имя тикера
-        # нашел макс и мин, сравнил если одинаковые то имя тикера добавил в список зеро
-        # получил полусумму, получил разность
-        # высчитал волатильность - добавил в новый словарь? или заменил значение в словаре из препаре? или
-        # создал 2 списка - список тикеров и список значений волатильности - как связать два списка?
-        # или добавил попарные значения в списки или в defaultdict 1 и 2?
-        # сравнил значения списке нашел мин, макс - сет негодится он удалит одинаковые значения
-
-    def prepare(self, file):  # предобработка данных
-        with open(file, newline='', encoding='utf-8') as file:
-            reader = csv.reader(file, delimiter=',')
-            count = 0
-            for line in reader:
-                if count == 0:
-                    count = 1
-                    continue
-                self.secid, self.tiker_price = line[0], line[2]
-                self.tiker_price_list.append(self.tiker_price)
-            # return self.secid, self.tiker_price_list
-
-            self.dict_file.update({self.secid: self.tiker_price_list})
-        return self.dict_file
-
-        # посчитать количество файлов в исходной папке
-        # открыть файл,
-        # прочитать все строки построчно, взять secid и price и добавить в словарь
-        # dict(self.secid = name, self.tiker_price = self.tiker_price.append(price)) (как вариант значения это список)
-        # self.dict_file.update(self.tiker_price, self.secid)
-        # dict.fromkeys(seq[, value])
-        # закрыть файл
-        # вернуть словарь (имя : список значений)
-
-    def get_value(self, dic, value):
-        for self.key in dic:
-            if dic[self.key] == value:
-                return self.key
-
-# TODO Старайтесь рабочий код оборачивать в if __name__ == '__main__'
-# TODO С процессами на виндоус это вообще необходимая деталь, а так это просто хороший тон
-# TODO чтобы код запускался только если модуль запускается явно
-volatil = Volatility()
-
-volatil.run(dir_name='trades')
+# for file_path in file_to_path(folder_name='trades'):
+#     print(file_path)
