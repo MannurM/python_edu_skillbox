2f71559918e4178eb2a2e5645ac3f131a837c0dc
Максим <bi4ok23@gmail.com>
2021-03-07 16:27:56 +0300

    lesson 014: нужны правки в 03

--- lesson_014/03_rules.py
+++ lesson_014/03_rules.py
@@ -36,9 +36,11 @@ def create_parser():
     parser = argparse.ArgumentParser(description='bowling_file')
     parser.add_argument('--file_input', type=str, required=True, help='file_data_bowling')
     parser.add_argument('--file_output', type=str, required=True, help='file_data_bowling')
+    # TODO Нужен ещё один аргумент, который будет отвечать за выбор правил
     return parser
 
-
+# TODO в этом модуле должен быть только парсер, остальные функции надо вынести
+# TODO + в целом новые функции не нужны, должны быть расширены старые, чтобы добавилась возможность выбора правил
 def open_file_tour(file_input, file_output):
     file_name = file_input
     file_output = file_output
@@ -102,3 +104,24 @@ if __name__ == '__main__':
     parser = create_parser()
     args = parser.parse_args('--file_input tournament.txt --file_output tournament_result_eu.txt'.split())
     open_file_tour(file_input=file_input, file_output=file_output)
+
+# TODO Расчёты по новым правилам(чтобы подправить свой алгоритм расчётов):
+# Новые правила
+# 811/X--3/XX171/43
+# 9 + 10+10 + 10+0+0 + 0 + 10+10 + 10+10+1 + 10+8 + 8 + 10+4 + 7 = 100 + 27
+#
+# ### Tour 1
+# Антон	1/6/1/--327-18812382     Недопустимая комбинация фрейма - «82»
+# Елена	3532X332/3/62--62X       90
+# Роман	725518X--8/--543152      Недопустимая комбинация фрейма - «55»
+# Татьяна	8/--35-47/371/518-4/ Недопустимая комбинация фрейма - «37»
+# Ринат	4-3/7/3/8/X711627-5      119
+# winner is Ринат
+#
+# ### Tour 2
+# Татьяна	42X--3/4/2-8271171/  Недопустимая комбинация фрейма - «82»
+# Роман	811/X--3/XX171/43        127
+# Ринат	-263X815/5/27-----6      81
+# Алексей	--8-X3/4/1/-12651X   88
+# Павел	3-6/5/9/5---1/--5-52     79
+# winner is Роман
